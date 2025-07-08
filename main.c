351b0345d225175741de072ead0a7568ced98f34
fatalitirar <fatalitirar@gmail.com>
2025-07-08 12:01:24 +0300

    prototipe v1

--- main.c
+++ main.c
@@ -6,9 +6,10 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -16,28 +17,62 @@
 #define PIPE_NAME "/tmp/tomato_pipe"
 #define TEST_MODE 99
 
-int wait = 0;
+static int app_mode = 0;
+static int state = 0;
 
-void clockTimer(int time) {
-  while (time) {
-    if (wait)
-      continue;
-    printf("%d : %d\n", time / 60, time % 60);
-    sleep(1);
-    clearScreen();
-    time--;
+pthread_mutex_t stop;
+pthread_mutex_t state_mu;
+
+void drop_app(int sig) {
+  if (app_mode == MODE_CLOCK) {
+    unlink(PIPE_NAME);
+    exit(EXIT_SUCCESS);
   }
 }
 
-void *initMode(void *arg) {
+void clockTimer(int time) {
+  int n_time = time;
+  while (n_time) {
+     clearScreen();
+     printf("%02d : %02d\n", n_time / 60, n_time % 60);
+        //printf("%d\n",state); 
+    switch (state) {
+    case S_STOP:
+      puts("rich lock");
+      pthread_mutex_lock(&stop);
+      break;
+    case S_PROGRESS:
+      sleep(1);
+      n_time--;
+      break;
+    case S_RESTART:
+      n_time = time;
+      state = S_PROGRESS;
+      break;
+    case S_SKIP:
+      state = S_PROGRESS;
+         return;
+      break;
+    case S_KILL:
+      kill(getpid(), SIGINT);
+      break;
+    }
+  }
+}
+
+void *init_mode(void *arg) {
   int mode = (int)(intptr_t)arg;
   int work_time = 0;
   int rest_time = 0;
+  state = S_PROGRESS;
+
+  pthread_mutex_init(&stop, NULL);
+  pthread_mutex_init(&state_mu,  NULL);
 
   switch (mode) {
   case 1:
     work_time = 25 * 60;
-    rest_time = 25 * 60;
+    rest_time = 5 * 60;
     break;
   case TEST_MODE:
     work_time = 5;
@@ -54,38 +89,59 @@ void *initMode(void *arg) {
   return 0;
 }
 
-void initTimer() {
+void init_timer() {
 
   pthread_t clock;
-  pthread_create(&clock, NULL, initMode, (void *)(intptr_t)TEST_MODE);
+  pthread_create(&clock, NULL, init_mode, (void *)(intptr_t)1);
 }
 
-void readFromTomatoPipe() {
+void read_from_tomato_pipe() {
   int fd = open(PIPE_NAME, O_NONBLOCK | O_RDONLY, 0664);
-  if (fd == -1){
-      perror("can't open to read pipe");
+  if (fd == -1) {
+    perror("can't open to read pipe");
   }
-  int number = 0;
-  int rdn = 0;
+  char cmd;
+  int rd;
   while (1) {
-    rdn = read(fd, &number, sizeof(int));
-    if (rdn) {
-      wait = number;
+    rd = read(fd, &cmd, sizeof(char));
+    if (rd){
+    switch (cmd) {
+    case 's':
+      state = S_STOP;
+      break;
+    case 'c':
+      state = S_PROGRESS;
+      pthread_mutex_unlock(&stop);
+      break;
+    case 'k':
+      state = S_KILL;
+      pthread_mutex_unlock(&stop);
+      break;
+    case 'r':
+      state = S_RESTART;
+      pthread_mutex_unlock(&stop);
+      break;
+    case 'p':
+      state = S_SKIP;
+      pthread_mutex_unlock(&stop);
+      break;
+    }
     }
   }
+  close(fd);
 }
 
-void writeToTomatoPipe(int number) {
-  int fd = open(PIPE_NAME, O_WRONLY | O_NONBLOCK);
+void write_to_tomato_pipe(char cmd) {
+  int fd = open(PIPE_NAME, O_WRONLY);
   if (fd) {
-    int f = write(fd, &number, sizeof(int));
+    int f = write(fd, &cmd, sizeof(char));
   } else {
     perror("can't write to pipe");
   }
+  close(fd);
 }
 
-// return pipe fd
-void initPipe() {
+void init_pipe() {
 
   int fd = open(PIPE_NAME, O_RDONLY | O_NONBLOCK);
   if (fd == -1) {
@@ -107,7 +163,7 @@ void initPipe() {
 }
 
 int main(int argc, char **argv) {
-
+  signal(SIGINT, drop_app);
   int opt = 0, fd = 0;
 
   while ((opt = getopt(argc, argv, "ic:h")) != -1) {
@@ -117,15 +173,20 @@ int main(int argc, char **argv) {
       print_help();
       break;
     case 'i':
-      initTimer();
-      initPipe();
-      readFromTomatoPipe();
+      app_mode = MODE_CLOCK;
+      init_pipe();
+      init_timer();
+      read_from_tomato_pipe();
       close(fd);
       pthread_exit(NULL);
       break;
     case 'c':
-      initPipe();
-      writeToTomatoPipe(atoi(optarg));
+      if (strlen(optarg) == 1) {
+        app_mode = MODE_CONTROL;
+        write_to_tomato_pipe(optarg[0]);
+      } else {
+        puts("wrong control input");
+      }
       break;
     }
     exit(EXIT_SUCCESS);
--- tomatoInLine.h
+++ tomatoInLine.h
@@ -1,13 +1,22 @@
 #pragma once
 #include <stdio.h>
 
+
+#define S_PROGRESS 1
+#define S_STOP 2
+#define S_KILL 3
+#define S_RESTART 4
+#define S_SKIP 5
+
+
+#define MODE_CLOCK 1
+#define MODE_CONTROL 2
+
 void print_help(){
    puts("toamto have two modes");
    puts("-i {n} where n is number of mode");
    puts("-c to controll");
 }
 
-typedef struct Tomato{
-   int state;
-   int mode;
-}Tomato;
+
+
